06e23b190bfcda35cff6a5c921debc591bd6e4a8
James Lewel Padecio <jameslewel.padecio@cit.edu>
2023-03-08 23:59:48 +0800

    Arrange code

--- Logic Gates/Program.cs
+++ Logic Gates/Program.cs
@@ -9,41 +9,13 @@ namespace Logic_Gates
             int[][] inputs = new int[4][];
             int[] outputs = new int[4];
 
-            // Pre-defined inputs
+            // Pre-defined inputs x
             inputs[0] = new int[2] { 0, 0 };
             inputs[1] = new int[2] { 0, 1 };
             inputs[2] = new int[2] { 1, 0 };
             inputs[3] = new int[2] { 1, 1 };
 
-            /* METHOD 1
-            //Takes user input of x
-            Console.WriteLine("Input X |X1|X2 - Separated by comma - {1,0}");
-            for (int i = 0; i < 4; i++)
-            {
-                inputs[i] = new int[2];
-                Console.Write("      x{0}| ", i);
-
-                // Reads and splits line by comma
-                string[] input = Console.ReadLine().Split(',');
-
-                // Loops and stores input into inputs
-                for (int j = 0; j < 2; j++)
-                    inputs[i][j] = int.Parse(input[j]);
-            }*/
-
-            /* METHOD 2
-            //Takes user input of y
-            Console.WriteLine("\n-------------------------------\n");
-            Console.WriteLine("Input Y | (Expected Output)");
-            for (int i = 0; i < 4; i++)
-            {
-                Console.Write("      y{0}| ", i);
-
-                outputs[i] = int.Parse(Console.ReadLine());
-            }
-            */
-
-            // METHOD 3
+            // Pre-defined outputs y
             Console.WriteLine("1 - [OR]");
             Console.WriteLine("2 - [AND]");
             Console.WriteLine("3 - [NOR]");
@@ -52,10 +24,10 @@ namespace Logic_Gates
             Console.WriteLine("6 - [XNOR]");
             Console.Write("Enter : ");
 
-            switch(Console.ReadLine())
+            switch (Console.ReadLine())
             {
                 case "1":
-                    outputs = new int[] { 0, 1, 1, 1};
+                    outputs = new int[] { 0, 1, 1, 1 };
                     break;
                 case "2":
                     outputs = new int[] { 0, 0, 0, 1 };
@@ -73,6 +45,34 @@ namespace Logic_Gates
                     outputs = new int[] { 1, 0, 0, 1 };
                     break;
             }
+
+            /* User-inputted METHOD
+            //Takes user input of x
+            Console.WriteLine("Input X |X1|X2 - Separated by comma - {1,0}");
+            for (int i = 0; i < 4; i++)
+            {
+                inputs[i] = new int[2];
+                Console.Write("      x{0}| ", i);
+
+                // Reads and splits line by comma
+                string[] input = Console.ReadLine().Split(',');
+
+                // Loops and stores input into inputs
+                for (int j = 0; j < 2; j++)
+                    inputs[i][j] = int.Parse(input[j]);
+            }*/
+
+            /* User-inputted METHOD
+            //Takes user input of y
+            Console.WriteLine("\n-------------------------------\n");
+            Console.WriteLine("Input Y | (Expected Output)");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("      y{0}| ", i);
+
+                outputs[i] = int.Parse(Console.ReadLine());
+            }
+            */
 
             Console.WriteLine("\n-------------------------------\n");
 
